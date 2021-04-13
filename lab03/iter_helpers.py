63db8be661f9fde82cb951627a705df6333f77a5
xhx0095 <71507775+xhx0095@users.noreply.github.com>
2021-04-13 21:56:31 +0800

    Create iter_helpers.py

---
+++ lab03/iter_helpers.py
@@ -0,0 +1,13 @@
+import itertools
+def transpose(a):
+    b = zip(* a)
+    b = list(b)
+    return b
+
+def scalar_product(a,b):
+    try:
+        a1=map(lambda x,y:int(x)*int(y),a,b)
+        a2=sum(list(a1))
+        return a1
+    except Valueerror:
+        return None
