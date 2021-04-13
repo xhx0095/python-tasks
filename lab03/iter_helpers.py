73008a7e58c5bfe6c71bc909259298b1b63f8748
xhx0095 <71507775+xhx0095@users.noreply.github.com>
2021-04-14 05:24:17 +0800

    Update iter_helpers.py

--- lab03/iter_helpers.py
+++ lab03/iter_helpers.py
@@ -8,6 +8,6 @@ def scalar_product(a,b):
     try:
         a1=map(lambda x,y:int(x)*int(y),a,b)
         a2=sum(list(a1))
-        return a1
+        return a2
     except Valueerror:
         return None
